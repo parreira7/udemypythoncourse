a6a2c8bc74a361ae7a977a9c98b1cdcee8a2d225
Gabriel <99111916+parreira7@users.noreply.github.com>
2022-03-16 04:29:14 -0300

    Update basicexercises.py

--- basicexercises.py
+++ basicexercises.py
@@ -340,5 +340,21 @@ print("Você nasceu em:", ano_idade)
 """
 print("Exercício 51")
 print("Leitor de coordenadas x e y no ponto R² de origem!\n")
-x = float(input("Insira a coordenada x: "))
-y = float(input("Insira a coordenada y: "))
+x1 = int(input(f'Informe x1 => '))
+y1 = int(input(f'Informe y1 => '))
+distancia = int((((0 - x1) ** 2) + ((0 - y1) ** 2)) ** (1 / 2))
+
+print(f'A distância entre a origem (0,0) e o ponto de'
+
+     f' coordenadas ({x1},{y1}) é: {distancia}')
+#################################################
+
+print(f'x2 - x1 = {0 - x1}, \n'
+
+     f'y2 - y1 = {0 - y1}')
+
+print(f'(x2 - x1) ** 2 = {(0 - x1) ** 2}, \n'
+
+     f'(y2 - y1) ** 2 = {(0 - y1) ** 2}')
+
+print(f'Distância da origem = {distancia}')
